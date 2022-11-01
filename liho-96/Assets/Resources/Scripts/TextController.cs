97a7b0ba2b79b6fa212db729f74983d1213bf096
Daniil Beloglazov <beloglazov3791@gmail.com>
2022-11-02 00:04:57 +0600

    first developments

---
+++ liho-96/Assets/Resources/Scripts/TextController.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class TextController : MonoBehaviour
+{
+
+    public float secondsBeforeNextSymbol = 0.1f;
+
+    private string[] _phrases =
+    {
+        "Молчаливый курьер везёт груз. Он никогда не спрашивает, что и кому он доставляет. За это его и ценят - и, возможно, лишь поэтому он до сих пор не получил пулю в лоб.",
+        "А это вторая фраза.",
+        "А вот и третья (последняя)."
+    };
+
+    private int _currentPhraseIndex;
+
+    private char[] _currentPhraseChars;
+
+    private string _currentPhrase;
+
+    private string _currentPhraseFinal;
+
+    private bool _isPrinting;
+
+    private TextMeshProUGUI _text;
+
+    private void Start()
+    {
+        _text = GetComponent<TextMeshProUGUI>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            UpdatePhrase();
+        }
+    }
+
+    private void UpdatePhrase()
+    {
+        if (_isPrinting)
+        {
+            _text.text = _currentPhraseFinal;
+            _isPrinting = false;
+            return;
+        }
+        
+        _currentPhraseFinal = _phrases[_currentPhraseIndex];
+        _currentPhraseChars = _currentPhraseFinal.ToCharArray();
+        _currentPhrase = "";
+        _currentPhraseIndex++;
+        _isPrinting = true;
+        StartCoroutine(PrintNextPhrase());
+    }
+
+    private IEnumerator PrintNextPhrase()
+    {
+        foreach (var currentChar in _currentPhraseChars)
+        {
+            yield return new WaitForSeconds(secondsBeforeNextSymbol);
+            if (!_isPrinting)
+            {
+                yield break;
+            }
+            _currentPhrase += currentChar;
+            _text.text = _currentPhrase;
+        }
+    }
+}
