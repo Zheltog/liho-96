276703c54e8fb807b00fff30113a8d6698c53c3d
d.mazhuga <->
2022-11-04 15:31:59 +0600

    image controller

---
+++ liho-96/Assets/Resources/Scripts/ImageController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UIElements;
+
+public class ImageController : MonoBehaviour
+{
+    private Image _image;
+
+    private void Start()
+    {
+        _image = GetComponent<Image>();
+        SetImage("car_driving_on_a_rainy_road");
+    }
+    
+    public void SetImage(string imageName)
+    {
+        var sprite = Resources.Load<Texture2D>("Images/" + imageName);
+        _image.image = sprite;
+    }
+}
