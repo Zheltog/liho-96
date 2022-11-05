c828a6da7b5c5763052b2d1d78432caf1fbbe230
Daniil Beloglazov <beloglazov3791@gmail.com>
2022-11-05 16:35:53 +0600

    adding "> " before button text

--- liho-96/Assets/Resources/Scripts/ButtonsBundle.cs
+++ liho-96/Assets/Resources/Scripts/ButtonsBundle.cs
@@ -20,8 +20,13 @@ public class ButtonsBundle : MonoBehaviour
     
     public void SetTexts(string text1, string text2, string text3)
     {
-        textOnButton1.text = text1;
-        textOnButton2.text = text2;
-        textOnButton3.text = text3;
+        textOnButton1.text = FormattedText(text1);
+        textOnButton2.text = FormattedText(text2);
+        textOnButton3.text = FormattedText(text3);
+    }
+
+    private static string FormattedText(string original)
+    {
+        return "> " + original;
     }
 }
