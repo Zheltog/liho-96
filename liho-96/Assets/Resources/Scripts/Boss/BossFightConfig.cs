fc97caf643cc85f5afb3cbc03ffa7b56664162e6
Daniil Beloglazov <beloglazov3791@gmail.com>
2022-11-05 23:55:01 +0600

    developments for boss fight config

---
+++ liho-96/Assets/Resources/Scripts/Boss/BossFightConfig.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+
+public class BossFightConfig
+{
+    public List<Phase> Phases { get; set; }
+    public List<Item> Items { get; set; }
+}
+
+public class Phase
+{
+    public PhaseType Type { get; set; }
+    public string StartText { get; set; }
+    public Effect Effect { get; set; }
+    public List<EnemyType> Enemies { get; set; }
+    public List<Modifier> Modifiers { get; set; }
+}
+
+public enum PhaseType
+{
+    Shooting
+}
+
+public class Effect
+{
+    public EffectType Type { get; set; }
+    public float Value { get; set; }
+}
+
+public enum EffectType
+{
+    Heal, Damage, Timer
+}
+
+public enum EnemyType
+{
+    BenchLeft, BenchRight
+}
+
+public enum Modifier
+{
+    Shake, Dark
+}
+
+public class Item
+{
+    public string Name { get; set; }
+    public string UseText { get; set; }
+    public string Flag { get; set; }
+    public Effect Effect { get; set; }
+}
---
+++ liho-96/Assets/Resources/Scripts/Boss/BossFightStateHolder.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+public static class BossFightStateHolder
+{
+    public static List<Item> AvailableItems { get; set; }
+    
+    public static List<Phase> Phases { get; set; }
+
+    public static Phase CurrentPhase;
+
+    public static void Init(List<Item> items, List<Phase> phases)
+    {
+        AvailableItems = items;
+        Phases = phases;
+    }
+}
