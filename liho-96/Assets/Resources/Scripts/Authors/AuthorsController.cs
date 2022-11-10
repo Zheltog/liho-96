ec6a0d022f6a4dee072cfd07b229f351ce7d4257
Daniil Beloglazov <beloglazov3791@gmail.com>
2022-11-11 00:46:11 +0600

    authors scene: scrollable text + hidden elements

---
+++ liho-96/Assets/Resources/Scripts/Authors/AuthorsController.cs
@@ -0,0 +1,22 @@
+﻿using Final;
+using UnityEngine;
+
+namespace Authors
+{
+    public class AuthorsController : MonoBehaviour
+    {
+        public GameObject rolesOpened;
+        public GameObject rolesClosed;
+        public GameObject secretGuestOpened;
+        public GameObject secretGuestClosed;
+
+        private void Start()
+        {
+            var isGameFinished = Final.StateHolder.CurrentState == State.Final;
+            rolesOpened.SetActive(isGameFinished);
+            rolesClosed.SetActive(!isGameFinished);
+            secretGuestOpened.SetActive(isGameFinished);
+            secretGuestClosed.SetActive(!isGameFinished);
+        }
+    }
+}
