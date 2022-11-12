0cfda8970f3f144fd109a591dcdaa32e054ae505
Daniil Beloglazov <beloglazov3791@gmail.com>
2022-11-12 20:45:45 +0600

    buttons texts underlined on mouse over

---
+++ liho-96/Assets/Resources/Scripts/Common/AuthorsButtonController.cs
@@ -0,0 +1,29 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Common
+{
+    public class AuthorsButtonController : MonoBehaviour
+    {
+        private TextMeshProUGUI _text;
+        public Color colorOnMouseOver = Color.white;
+
+        private Color _originalColor;
+
+        private void Start()
+        {
+            _text = GetComponentInChildren<TextMeshProUGUI>();
+            _originalColor = _text.color;
+        }
+        
+        public void OnMouseOver()
+        {
+            _text.color = colorOnMouseOver;
+        }
+
+        public void OnMouseExit()
+        {
+            _text.color = _originalColor;
+        }
+    }
+}
---
+++ liho-96/Assets/Resources/Scripts/Common/ButtonTextStyleController.cs
@@ -0,0 +1,25 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Common
+{
+    public class ButtonTextStyleController : MonoBehaviour
+    {
+        private TextMeshProUGUI _text;
+
+        private void Start()
+        {
+            _text = GetComponentInChildren<TextMeshProUGUI>();
+        }
+        
+        public void OnMouseOver()
+        {
+            _text.fontStyle = FontStyles.Underline;
+        }
+
+        public void OnMouseExit()
+        {
+            _text.fontStyle = FontStyles.Normal;
+        }
+    }
+}
